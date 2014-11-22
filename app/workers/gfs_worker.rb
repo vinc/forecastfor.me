e4f6d4f3095c6755d31d45d1910f87d6eb6bd840
Vincent Ollivier <v@vinc.cc>
2014-11-22 13:49:24 +0100

    Upate GFSWorker

--- app/workers/gfs_worker.rb
+++ app/workers/gfs_worker.rb
@@ -5,6 +5,8 @@ class GFSWorker
   recurrence { hourly }
 
   def perform
-    GFS.new.download!
+    last_run_time = Bulletin.run_time(Time.now)
+    yyyymmddhh = last_run_time.strftime('%Y%m%d%H')
+    GFS.find_or_create(yyyymmddhh)
   end
 end
