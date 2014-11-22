26e1a410d15cba1fe39e873488a16b46faf31dc7
Vincent Ollivier <v@vinc.cc>
2014-11-22 22:06:23 +0100

    Add lock to BulletinWorker

--- app/workers/bulletin_worker.rb
+++ app/workers/bulletin_worker.rb
@@ -3,9 +3,14 @@ class BulletinWorker
 
   def perform(str, longitude, latitude)
     key = [str, longitude, latitude].join(':')
+
+    # Acquire lock
+    return false if Redis.current.exists("#{key}:lock")
+    Redis.current.setex("#{key}:lock", 1.minute, 'busy')
+
     date = Time.zone.parse(str).to_date
     bulletin = Bulletin.new(date, longitude: longitude, latitude: latitude)
     %w(weather temperature wind).each { |method| bulletin.send(method) }
-    Redis.current.setex(key, 1.hour, 'ok')
+    Redis.current.setex(key, 1.hour, 'done')
   end
 end
