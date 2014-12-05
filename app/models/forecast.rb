vinc/forecastfor.me

commit d062a7b9675eca0e39af78e06cca1bc54575f035

Keep a forecast for every hour of bulletin

app/models/forecast.rb
@@ -24,7 +24,8 @@ class Forecast
   end
 
   def precipitations
-    (self.read(:prate) * 3.hours).round(1)
+    # Amount of precipitations during one hour
+    self.read(:prate).round(1)
   end
 
   def temperature

app/models/bulletin.rb
@@ -37,7 +37,7 @@ class Bulletin
 
       gfs = GFS.find_or_create(time.strftime('%Y%m%d%H'))
       gfs.forecast(hour: hour, longitude: longitude, latitude: latitude)
-    end.uniq # FIXME: extrapolate for every hour
+    end
   end
 
   def location

commit 421324ab340e8a54b59dcdd2f065268384297bbf

Fix bug introduced by commit d062a7b

app/models/forecast.rb
@@ -24,8 +24,7 @@ class Forecast
   end
 
   def precipitations
-    # Amount of precipitations during one hour
-    self.read(:prate).round(1)
+    (self.read(:prate) * 1.hour).round(1)
   end
 
   def temperature